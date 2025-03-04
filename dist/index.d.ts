4b997dbc5bea826a32ab7cfda9c67226eb6bdfe2
Thiago <thiago_sl2@htomail.com>
2025-03-04 17:19:58 -0300

    feature: added method

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,11 +3,12 @@ import RedisCache from "./services/RedisHandler.ts";
 declare class Authenticate extends RedisCache {
     protected connect(): Promise<void>;
     private userLogged;
-    db_connection: string;
+    db_connection: string | null;
     private secretKey;
     constructor(userLogged: UserLogger, databaseURL: string);
     logIn(): Promise<void>;
     logOut(): Promise<void>;
     authorize(): Promise<boolean>;
+    static authorizationTemporary(secret: string): Promise<string>;
 }
 export default Authenticate;
